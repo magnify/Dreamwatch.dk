69273bd02a2bcb4d68f9708db3de6b254ab3f68d
Andreas Krohn <andreas.kian@gmail.com>
2012-06-07 16:30:46 +0200

    Added Facebook JS SDK to page template

--- sites/default/themes/dreamwatch/page.tpl.php
+++ sites/default/themes/dreamwatch/page.tpl.php
@@ -93,6 +93,15 @@
 </head>
 <body class="<?php print $body_classes; ?>">
 
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/<?php print ($language->language == 'da' ? 'da_DK' : 'en_US'); ?>/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
   <div id="page"><div id="page-inner">
 
     <a name="navigation-top" id="navigation-top"></a>
